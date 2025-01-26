98039a244e15655f72d4048734f0c71ff9acf687
Antonio Ventilii <antonioventilii@gmail.com>
2025-01-26 18:36:17 +0100

    feat: add custom exceptions

--- deribit_wrapper/exceptions.py
+++ deribit_wrapper/exceptions.py
@@ -1,2 +1,29 @@
 class DeribitClientWarning(Warning):
     pass
+
+class InvalidParameterError(Exception):
+    pass
+
+
+class InvalidMarginModelError(Exception):
+    pass
+
+
+class WaitRequiredError(Exception):
+    pass
+
+
+class SubaccountError(Exception):
+    pass
+
+class SubaccountNameAlreadyTaken(SubaccountError):
+    pass
+
+class SubaccountNameWrongFormat(SubaccountError):
+    pass
+
+class SubaccountNotRemovable(SubaccountError):
+    pass
+
+class SubaccountAlreadyRemoved(SubaccountError):
+    pass

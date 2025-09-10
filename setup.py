955448d82a83dfeff2e7a94143dc35c90cfd669b
Antonio Ventilii <antonioventilii@gmail.com>
2025-09-10 20:06:55 +0200

    release: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='deribit_wrapper',
-    version='0.4.0',
+    version='0.4.1',
     packages=find_packages(),
     description='A Python wrapper for seamless integration with Deribit\'s trading API, offering easy access to '
                 'market data, account management, and trading operations.',

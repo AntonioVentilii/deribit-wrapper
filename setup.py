10819ad13612dae0740e1a61c7f31fb42779b9a7
Antonio Ventilii <antonioventilii@gmail.com>
2026-05-29 19:48:05 +0200

    bump: version 0.5.0 (#61)
    Bump version to 0.5.0 in setup.py to prepare the release.

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='deribit_wrapper',
-    version='0.4.2',
+    name="deribit_wrapper",
+    version="0.5.0",
     packages=find_packages(),
-    description='A Python wrapper for seamless integration with Deribit\'s trading API, offering easy access to '
-                'market data, account management, and trading operations.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Antonio Ventilii',
-    author_email='antonioventilii@gmail.com',
-    license='MIT',
+    description="A Python wrapper for seamless integration with Deribit's trading API, offering easy access to "
+    "market data, account management, and trading operations.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Antonio Ventilii",
+    author_email="antonioventilii@gmail.com",
+    license="MIT",
     install_requires=[
-        'requests',
-        'numpy',
-        'pandas',
-        'urllib3',
-        'progressbar2',
-        'cryptography>=42.0.0',
+        "requests",
+        "numpy",
+        "pandas",
+        "urllib3",
+        "progressbar2",
+        "cryptography>=42.0.0",
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
-    url='https://github.com/AntonioVentilii/deribit-wrapper',
+    python_requires=">=3.10",
+    url="https://github.com/AntonioVentilii/deribit-wrapper",
     project_urls={
-        'Source Code': 'https://github.com/AntonioVentilii/deribit-wrapper',
-        'Issue Tracker': 'https://github.com/AntonioVentilii/deribit-wrapper/issues',
+        "Source Code": "https://github.com/AntonioVentilii/deribit-wrapper",
+        "Issue Tracker": "https://github.com/AntonioVentilii/deribit-wrapper/issues",
     },
-    keywords='deribit api wrapper cryptocurrency trading',
+    keywords="deribit api wrapper cryptocurrency trading",
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # Define console scripts here if needed
         ],
     },
